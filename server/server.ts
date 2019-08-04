8cc0d540f5ca4b7fe941ed215a7fe727144fc6e9
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2019-08-03 22:57:04 -0300

    implementing roles, tests are broken btw

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs'
+
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
 
@@ -5,7 +7,8 @@ import {environment} from '../common/environment'
 import {Router} from '../common/router'
 import {mergePatchBodyParser} from './merge-patch.parser'
 import {handleError} from './error.handler'
-import { tokenParser } from '../security/token.parser'
+
+import {tokenParser} from '../security/token.parser'
 
 export class Server {
 
@@ -22,10 +25,16 @@ export class Server {
     return new Promise((resolve, reject)=>{
       try{
 
-        this.application = restify.createServer({
+        const options: restify.ServerOptions = {
           name: 'meat-api',
-          version: '1.0.0'
-        })
+          version: '1.0.0',
+        }
+        if(environment.security.enableHTTPS){
+          options.certificate = fs.readFileSync(environment.security.certificate),
+          options.key = fs.readFileSync(environment.security.key)
+        }
+
+        this.application = restify.createServer(options)
 
         this.application.use(restify.plugins.queryParser())
         this.application.use(restify.plugins.bodyParser())
@@ -55,7 +64,7 @@ export class Server {
   }
 
   shutdown(){
-    return mongoose.disconnect().then(() => this.application.close())
+    return mongoose.disconnect().then(()=>this.application.close())
   }
 
 }
